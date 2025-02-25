043afa9e0b76a27f0509d96d0230b32e4c6e7e14
Ajay Barman <barmanjiaj@gmail.com>
2025-02-25 10:31:05 +0530

    prac

--- experimentsWTs/src/index.ts
+++ experimentsWTs/src/index.ts
@@ -1,7 +1,22 @@
-class User { // BETTER WAY
-    private city: string = "jaipur"
-    constructor(public email: string, public name: string) {
+class User {
+    // BETTER WAY
+    protected kitty: string = "meow"
+    private city: string = "jaipur";
+    constructor(
+        public email: string,
+        public name: string,
+    ) {}
+
+    // getter & setter
+    get getAppleEmail(): string {
+        return `email: ${this.email}`;
     }
+    // set ----,,,
+}
+
+class SubUser extends User{
+    kitty: string = "pussy" // city cant be redifine as its a private NOTE: thats why we have PROTECTED, which is private for other varaible outside the scope but we can use in other classes
+    // Acquires all property except Private ones
 }
 
 // class User {
@@ -11,7 +26,7 @@ class User { // BETTER WAY
 //     constructor(email: string, name: string) {
 //         this.email = email;
 //         this.name = name;
-//     }
+//
 // }
 
 const barmanji = new User("abc@abc.com", "Abc");
